12dcd7ba9db07934c46a3aa07c78dd08127e336b
Ratatouille <307338947@qq.com>
2019-03-30 20:35:22 +0800

    顶部加入了一行注释

--- 04Day作业 结构体和指针的练习/04Day作业 结构体和指针的练习.cpp
+++ 04Day作业 结构体和指针的练习/04Day作业 结构体和指针的练习.cpp
@@ -1,8 +1,12 @@
-﻿#define _CRT_SECURE_NO_WARNINGS
+﻿/*
+	Test to commit
+*/
+#define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 
 typedef struct Teacher
